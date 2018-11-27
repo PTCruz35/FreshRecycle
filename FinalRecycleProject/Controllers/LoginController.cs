6b8da6d18fdf91ac86c33d1f4d4a81f036e7945a
uma <umathan13@gmail.com>
2018-11-26 20:26:58 -0500

    Modified Application Page, and Included forms for New User and Login

---
+++ FinalRecycleProject/Controllers/LoginController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FinalRecycleProject.Models;
+using System.Data;
+using System.Data.Entity;
+using System.Data.SqlClient;
+
+namespace FinalRecycleProject.Controllers
+{
+
+   
+    public class LoginController : Controller
+    {
+        private string DSN1 = System.Configuration.ConfigurationManager.AppSettings["connString"];
+
+        public string DSNCode()
+        {
+            return DSN1;
+        }
+        // GET: Login
+        // private DataModelContainer db = new DataModelContainer();
+        public ActionResult NewUser()
+        {
+
+          
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+        public ActionResult CreateOrUpdate(UserDetails user)
+        {
+            if(user !=null)
+            {
+               // SqlConnection con = new SqlConnection(this.DSNCode());
+                
+                // db.SaveChanges();
+               
+
+
+            }
+            return View(user);
+
+
+
+        }
+    }
+}
